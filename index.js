2df8451ec222693ea1b90eddd344c8b73f6076e2
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-01-02 19:43:13 +0600

    server

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", (req, res) => {
-  res.send("connected Server");
+  res.send("Aqurizzzzz Server connected");
 });
 
 client.connect((err) => {
